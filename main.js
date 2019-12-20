2372f05d8933662ed2e5196dba8cc9e4928a2ddd
MunkhdalaiSainbileg <dalai8888@gmail.com>
2019-12-20 12:48:29 +0000

    Fibonacci algorithm is added.

--- main.js
+++ main.js
@@ -21,5 +21,23 @@ function findFactorialIterative(number) {
   return answer;
 }
 
-findFactorialRecursive(5);
+// findFactorialRecursive(5);
 //findFactorialIterative(4);
+
+function fibonacciIterative(n){
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++){
+    arr.push(arr[i - 2] + arr[i -1]);
+  }
+ return arr[n];
+}
+
+function fibonacciRecursive(n) {
+  if (n < 2){
+    return n;
+  }
+  return fibonacciRecursive(n - 1) + fibonacciRecursive (n - 2)
+}
+
+// fibonacciIterative(3);
+// fibonacciRecursive(6)
