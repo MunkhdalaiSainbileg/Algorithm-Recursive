461201e9da9f26b2dc1026f66ca0409e8a3a0e07
MunkhdalaiSainbileg <dalai8888@gmail.com>
2019-12-23 10:50:22 +0000

    Dynamic programming is added.

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 function findFactorialRecursive(number) {
   // console.log(number)
   //code here
-  if(number > 1 ){
-     number = number * findFactorialRecursive(number-1);
-     //console.log(answer)
+  if (number > 1) {
+    number = number * findFactorialRecursive(number - 1);
+    //console.log(answer)
   }
   return number;
 }
@@ -13,7 +13,7 @@ function findFactorialRecursive(number) {
 function findFactorialIterative(number) {
   //code here
   let answer = 1;
-  while(number>1){
+  while (number > 1) {
     answer = answer * number;
     //console.log(number)
     number--;
@@ -24,19 +24,43 @@ function findFactorialIterative(number) {
 // findFactorialRecursive(5);
 //findFactorialIterative(4);
 
-function fibonacciIterative(n){
+function fibonacciIterative(n) {
   let arr = [0, 1];
-  for (let i = 2; i < n + 1; i++){
-    arr.push(arr[i - 2] + arr[i -1]);
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
   }
- return arr[n];
+  return arr[n];
 }
 
+let counter = 0;
+function fibonacciRecursiveWithMemoi() {
+  let array = {};
+  return function fib(n) {
+    counter++;
+    if (n in array) {
+      return array[n];
+    } else {
+      if (n < 2) {
+        array[n] = n;
+        return n;
+      }
+      array[n] = fib(n - 1) + fib(n - 2);
+      return array[n]
+    }
+  }
+}
+
+const memoized = new fibonacciRecursiveWithMemoi();
+
+console.log('5' + memoized(10));
+console.log('Number of operation: '+ counter);
+
+
 function fibonacciRecursive(n) {
-  if (n < 2){
+  if (n < 2) {
     return n;
   }
-  return fibonacciRecursive(n - 1) + fibonacciRecursive (n - 2)
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
 }
 
 // fibonacciIterative(3);
